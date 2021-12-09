a3d3ccfb2911d2cf022583c740ecf3a6208e12ad
chandlerblaschka <cblaschka@live.com>
2021-12-08 20:49:45 -0600

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -14,18 +14,6 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
-// SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex
-    })
-  } else {
-    res.send('404')
-  }
-})
-
 // CREATE
 breads.post('/', (req, res) => {
   if (!req.body.image) {
@@ -38,6 +26,37 @@ breads.post('/', (req, res) => {
   }
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+//EDIT
+breads.get('/:arrayIndex/edit', (req,res) => {
+  res.render('edit', {
+    bread: Bread[req.params.arrayIndex],
+    index: req.params.arrayIndex,
+  })
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 // DELETE
