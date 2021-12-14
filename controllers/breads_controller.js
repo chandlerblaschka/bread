be524831dfcb626e97c4284ea21d3f56bdee9f0b
chandlerblaschka <cblaschka@live.com>
2021-12-14 16:16:24 -0600

    added mongoose

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,10 +4,13 @@ const Bread = require('../models/bread.js');
 
 // INDEX
 breads.get('/', (req, res) => {
-  //res.send(Bread)
-  res.render("index", {
-    breads: Bread
-  });
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
 
 breads.get('/new', (req, res) => {
@@ -16,17 +19,18 @@ breads.get('/new', (req, res) => {
 
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread.push(req.body)
+  Bread.create(req.body)
   res.redirect('/breads')
 })
+
 
 //EDIT
 breads.get('/:arrayIndex/edit', (req,res) => {
@@ -37,16 +41,28 @@ breads.get('/:arrayIndex/edit', (req,res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex
+// breads.get('/:arrayIndex', (req, res) => {
+//   if (Bread[req.params.arrayIndex]) {
+//     res.render('Show', {
+//       bread:Bread[req.params.arrayIndex],
+//       index: req.params.arrayIndex
+//     })
+//   } else {
+//     res.send('404')
+//   }
+// })
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
     })
-  } else {
-    res.send('404')
-  }
+    .catch(err => {
+      res.send('404')
+    })
 })
+
 
 // UPDATE
 breads.put('/:arrayIndex', (req, res) => {
