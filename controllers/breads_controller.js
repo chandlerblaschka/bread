a9a6f438d029e1be371c96a1ccab65d615ff32ec
chandlerblaschka <cblaschka@live.com>
2021-12-15 18:54:22 -0600

    helper methods

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread.js');
+const seed = require('../models/seed.js');
 
 // INDEX
 breads.get('/', (req, res) => {
@@ -32,12 +33,28 @@ breads.post('/', (req, res) => {
 })
 
 
+//CREATE SEED 
+breads.get('/data/seed', (req, res) => {
+  Bread.insertMany(seed)
+    .then(createdBreads => {
+      res.redirect('/breads')
+    })
+})
+
 //EDIT
-breads.get('/:arrayIndex/edit', (req,res) => {
-  res.render('edit', {
-    bread: Bread[req.params.arrayIndex],
-    index: req.params.arrayIndex,
-  })
+// breads.get('/:arrayIndex/edit', (req,res) => {
+//   res.render('edit', {
+//     bread: Bread[req.params.arrayIndex],
+//     index: req.params.arrayIndex,
+//   })
+// })
+breads.get('/:id/edit', (req, res) => {
+  Bread.findById(req.params.id) 
+    .then(foundBread => { 
+      res.render('edit', {
+        bread: foundBread 
+      })
+    })
 })
 
 // SHOW
@@ -65,22 +82,41 @@ breads.get('/:id', (req, res) => {
 
 
 // UPDATE
-breads.put('/:arrayIndex', (req, res) => {
+// breads.put('/:arrayIndex', (req, res) => {
+//   if(req.body.hasGluten === 'on'){
+//     req.body.hasGluten = true
+//   } else {
+//     req.body.hasGluten = false
+//   }
+//   Bread[req.params.arrayIndex] = req.body
+//   res.redirect(`/breads/${req.params.arrayIndex}`)
+// })
+breads.put('/:id', (req, res) => {
   if(req.body.hasGluten === 'on'){
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  Bread[req.params.arrayIndex] = req.body
-  res.redirect(`/breads/${req.params.arrayIndex}`)
+  Bread.findByIdAndUpdate(req.params.id, req.body, { new: true }) 
+    .then(updatedBread => {
+      console.log(updatedBread) 
+      res.redirect(`/breads/${req.params.id}`) 
+    })
 })
+
+
 
 // DELETE
 //splice removes item from array
-breads.delete('/:arrayIndex', (req, res) => {
-  Bread.splice(req.params.arrayIndex, 1)
-  res.status(303).redirect('/breads')
+// breads.delete('/:arrayIndex', (req, res) => {
+//   Bread.splice(req.params.arrayIndex, 1)
+//   res.status(303).redirect('/breads')
+// })
+breads.delete('/:id', (req, res) => {
+  Bread.findByIdAndDelete(req.params.id) 
+    .then(deletedBread => { 
+      res.status(303).redirect('/breads')
+    })
 })
-
 
 module.exports = breads;
