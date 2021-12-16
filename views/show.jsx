357edcdbed64ed1b4df287c6a34df2960c2107c6
chandlerblaschka <cblaschka@live.com>
2021-12-15 20:56:22 -0600

    added instance method

--- views/show.jsx
+++ views/show.jsx
@@ -12,7 +12,7 @@ function Show({bread}){
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
-            <p>Baked by {bread.baker}</p>
+            <p>{bread.getBakedBy()}</p>
             <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
