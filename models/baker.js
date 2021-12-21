2eeaefe31fcbca2ab12f1572a67184f1ee2990a2
chandlerblaschka <cblaschka@live.com>
2021-12-20 18:35:08 -0600

    hook for deleting bakers and bread children

--- models/baker.js
+++ models/baker.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-const {Schema} = mongoose
+const { Schema } = mongoose
 const Bread = require('./bread')
 
 //Schema    
 
-const bakerSchema = new Schema ({
+const bakerSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -15,7 +15,7 @@ const bakerSchema = new Schema ({
         required: true
     },
     bio: String
-}, {toJSON: { virtuals: true}})
+}, { toJSON: { virtuals: true } })
 
 //virtuals:
 bakerSchema.virtual('breads', {
@@ -24,6 +24,13 @@ bakerSchema.virtual('breads', {
     foreignField: 'baker'
 })
 
+// hooks 
+bakerSchema.post('findOneAndDelete', function () {
+    Bread.deleteMany({ baker: this._conditions._id })
+        .then(deleteStatus => {
+            console.log(deleteStatus)
+        })
+})
 // model and export
 const Baker = mongoose.model('Baker', bakerSchema)
 module.exports = Baker
